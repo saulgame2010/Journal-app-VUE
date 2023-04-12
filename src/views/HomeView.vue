1fa950d5dbec490c92b8b22f2fea2cd27c8104a5
saulgame2010 <saulgame2010@gmail.com>
2023-04-12 17:28:31 -0600

    SASS styles for custom bootsrap theme were configured

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hola mundo</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-success">Success</button>
   </div>
 </template>
