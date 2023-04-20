e71bdd877db3e2f6d5afe3692853e49fcd7a5583
saulgame2010 <saulgame2010@gmail.com>
2023-04-20 17:49:26 -0600

    State was modified

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
     isLoading: true,
-    entries: [1, 2, 3, 4, 5]
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Anim deserunt sint pariatur do dolore dolor qui commodo in excepteur culpa adipisicing. Labore est nulla dolore ipsum in esse sunt velit nisi. Sit anim commodo consectetur eiusmod laborum. Lorem voluptate esse pariatur ut veniam. Et deserunt aliquip amet cillum aliquip anim ipsum anim dolore in consequat. Ut cillum ullamco nostrud sit officia elit cupidatat. Non id adipisicing fugiat enim mollit exercitation commodo aliquip eu elit dolor nisi incididunt.',
+            picture: null, //url para la imagen
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'In quis labore anim aliqua anim adipisicing aliquip. Nostrud minim do nisi sint elit proident proident sit duis. Eu eiusmod laboris adipisicing amet est irure cillum. Exercitation in cillum consequat tempor voluptate consequat ut reprehenderit non tempor fugiat anim officia. Consequat ipsum eiusmod reprehenderit anim est aliqua et.',
+            picture: null, //url para la imagen
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Do anim minim laborum occaecat pariatur fugiat irure ad esse nulla duis. Laborum ad incididunt nulla ea cillum occaecat consectetur laboris ipsum irure enim reprehenderit veniam. Qui cillum fugiat consequat duis commodo ut. Lorem ad ullamco commodo nulla.',
+            picture: null, //url para la imagen
+        },
+    ]
 })
